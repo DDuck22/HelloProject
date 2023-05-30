b03d1a84b020bbab83863d1639cbaa2460fa8b73
admin <admin@DESKTOP-486D1SK>
2023-05-30 10:41:07 +0900

    first commit.

---
+++ helloapp/src/com/yedam/NewWorld.java
@@ -0,0 +1,8 @@
+package com.yedam;
+
+public class NewWorld {
+    public static void main(String[] args) {
+        System.out.println("New World");
+        System.out.println("git 연동.");
+    }
+}
